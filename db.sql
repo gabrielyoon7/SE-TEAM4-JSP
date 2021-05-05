5f780c5cb3b179689e2ed5e2cb13d9cb7d283e88
gabrielyoon7 <gabrielyoon7@gmail.com>
2021-05-05 21:02:04 +0900

    DB UPLOAD

--- db.sql
+++ db.sql
@@ -5,53 +5,42 @@ CREATE DATABASE booksys ;
 USE booksys ;
 
 CREATE TABLE Oid (
-       last_id	 INT NOT NULL
+    last_id	 INT NOT NULL
 ) ;
 
 CREATE TABLE `Table` (
-       oid	     INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       number	 INT NOT NULL,
-       places	 INT NOT NULL
+                         oid	     INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                         number	 INT NOT NULL,
+                         places	 INT NOT NULL
 ) ;
 
 CREATE TABLE Customer (
-       oid	     INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       name	     VARCHAR(32) NOT NULL,
-       phoneNumber  CHAR(13) NOT NULL
+                          oid	     INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                          name	     VARCHAR(32) NOT NULL,
+                          phoneNumber  CHAR(13) NOT NULL
 ) ;
 
 CREATE TABLE WalkIn (
-       oid	     int NOT NULL PRIMARY KEY,
-       covers	 int,
-       date	     DATE,
-       time	     TIME,
-       table_id	 int
+                        oid	     int NOT NULL PRIMARY KEY,
+                        covers	 int,
+                        date	     DATE,
+                        time	     TIME,
+                        table_id	 int
 ) ;
 
 CREATE TABLE Reservation (
-       oid	        int NOT NULL PRIMARY KEY,
-       covers	    int,
-       date	        DATE,
-       time	        TIME,
-       table_id	    int,
-       customer_id  int,
-       arrivalTime  TIME
+                             oid	        int NOT NULL PRIMARY KEY,
+                             covers	    int,
+                             date	        DATE,
+                             time	        TIME,
+                             table_id	    int,
+                             customer_id  int,
+                             arrivalTime  TIME
 ) ;
-CREATE TABLE NOTICE
-( ID DOUBLE NOT NULL,
-TITLE NVARCHAR(100) NOT NULL,
-WRITER_ID NVARCHAR(50) NOT NULL,
-CONTENT LONGTEXT,
-REGDATE DATETIME (6) DEFAULT CURRENT_TIMESTAMP NOT NULL,
-HIT DOUBLE DEFAULT 0 NOT NULL,
-FILES NVARCHAR(1000),
-PUB TINYINT DEFAULT 0 NOT NULL,
-CONSTRAINT `NOTICE_PK` PRIMARY KEY (`ID`)
-);
-
-CREATE VIEW `NOTICE_VIEW` AS SELECT * FROM `NOTICE` ORDER BY REGDATE DESC;
-
 
 INSERT INTO `table` VALUES(101,1,1);
 INSERT INTO `table` VALUES(102,2,2);
 INSERT INTO `table` VALUES(103,3,3);
+INSERT INTO `customer` VALUES(0, 'Gab', '010-4413-5698');
+INSERT INTO `customer` VALUES(1, 'Arthur', '010-0000-0000');
+INSERT INTO `customer` VALUES(2, 'Dutch', '010-1111-1111');
