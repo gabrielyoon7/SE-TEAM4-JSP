eeb686269daeb29918f70988a8018ab85711a40a
gabrielyoon7 <gabrielyoon7@gmail.com>
2021-06-05 18:01:09 +0900

    [윤주현/UPDATE] TEST용 폴더 추가

---
+++ test/com/se/team4/application/persistency/DAO/Home/HomeDAOTest.java
@@ -0,0 +1,13 @@
+package com.se.team4.application.persistency.DAO.Home;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeDAOTest {
+
+    @org.junit.jupiter.api.Test
+    void loginCheck() {
+        HomeDAO homeDAO = new HomeDAO();
+        assertEquals(true, homeDAO.loginCheck("201713919"+"-/-/-"+"1234"));
+        assertEquals(true, homeDAO.loginCheck("201713919"+"-/-/-"+"0000"));
+    }
+}
