190bb448ee49ef183a5bb4e5d53c43aa59d757f6
gabrielyoon7 <gabrielyoon7@gmail.com>
2021-06-06 14:16:24 +0900

    [윤주현/UPDATE] TEST 작성중 (몇개 더 추가 예정)

--- test/com/se/team4/application/persistency/DAO/Home/HomeDAOTest.java
+++ test/com/se/team4/application/persistency/DAO/Home/HomeDAOTest.java
@@ -5,9 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class HomeDAOTest {
 
     @org.junit.jupiter.api.Test
-    void loginCheck() {
+    void loginCheck() { //로그인 테스트
         HomeDAO homeDAO = new HomeDAO();
-        assertEquals(true, homeDAO.loginCheck("201713919"+"-/-/-"+"1234"));
-        assertEquals(false, homeDAO.loginCheck("201713919"+"-/-/-"+"0000"));
+        assertTrue(homeDAO.loginCheck("201713919"+"-/-/-"+"1234"));
+        assertFalse(homeDAO.loginCheck("201713919"+"-/-/-"+"0000"));
     }
+
+    @org.junit.jupiter.api.Test
+    void signUp(){ //회원가입 시 중복회원 있는지 테스트
+        HomeDAO homeDAO = new HomeDAO();
+        assertEquals("실패", homeDAO.signUp("name"+"-/-/-"+"2020-01-01"+"-/-/-"+"201713919"+"-/-/-"+"0000"+"-/-/-"+"010-0000-0000"));
+    }
+
+
+
+
 }
