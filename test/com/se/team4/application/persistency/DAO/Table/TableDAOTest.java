21b7ae4908589b1767f2c10f778a30ae06969c86
gabrielyoon7 <gabrielyoon7@gmail.com>
2021-06-07 04:28:49 +0900

    [윤주현/FIX] 테스트 오류 수정

--- test/com/se/team4/application/persistency/DAO/Table/TableDAOTest.java
+++ test/com/se/team4/application/persistency/DAO/Table/TableDAOTest.java
@@ -3,24 +3,25 @@ package com.se.team4.application.persistency.DAO.Table;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class TableDAOTest {
     @Test
     void addTable() {
         TableDAO tableDAO = new TableDAO();
-        assertEquals("", tableDAO.addTable("3"+"-/-/-"+"3"+"-/-/-"+"5"));
+        assertEquals("", tableDAO.addTable("7"+"-/-/-"+"7"+"-/-/-"+"5"));
         //data =number+"-/-/-"+place+"-/-/-"+limits;
+    }
+
+    @Test
+    void modifyTable() {
+        TableDAO tableDAO = new TableDAO();
+        assertEquals("", tableDAO.modifyTable("7"+"-/-/-"+"7"+"-/-/-"+"7"+"-/-/-"+"7"));
+        //data =oid+"-/-/-"+number+"-/-/-"+places+"-/-/-"+"limits;
     }
     @Test
     void deleteTable() {
         TableDAO tableDAO = new TableDAO();
-        assertEquals("", tableDAO.deleteTable("3"));
-        //data =number+"-/-/-"+place+"-/-/-"+limits;
-    }
-    @Test
-    void modifyTable() {
-        TableDAO tableDAO = new TableDAO();
-        assertEquals("", tableDAO.modifyTable("3"));
-        //data =oid+"-/-/-"+number+"-/-/-"+places+"-/-/-"+"limits;
+        assertEquals("", tableDAO.deleteTable("7"));
     }
 }
